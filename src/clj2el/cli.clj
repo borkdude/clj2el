90e8d1523c6a4cd077250fac746b85d7a2934ec5
Teodor Heggelund <git@teod.eu>
2023-03-21 14:30:24 +0100

    Refactor: work on naming

--- src/clj2el/cli.clj
+++ src/clj2el/cli.clj
@@ -5,11 +5,12 @@
    [clojure.string :as str]
    [edamame.core :as e]))
 
-(defn pprint-str-multiple-forms [forms]
+(defn read-str-multiple [s]
+  (e/parse-string-all s {:all true}))
+
+(defn write-str-multiple [forms]
   (str/join "\n" (map #(with-out-str (pprint %)) forms)))
 
 (defn -main [& _args]
-  (let [read-str (fn [s] (e/parse-string-all s {:all true}))
-        clj2el-fn (partial map clj2el)
-        write-str pprint-str-multiple-forms]
-    (-> *in* slurp read-str clj2el-fn write-str print)))
+  (let [clj2el-fn (partial map clj2el)]
+    (-> *in* slurp read-str-multiple clj2el-fn write-str-multiple print)))
