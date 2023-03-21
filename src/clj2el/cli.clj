8be4c2dd6f5ad431ee99140cc20ed0b173c5c858
Teodor Heggelund <git@teod.eu>
2023-03-21 14:21:59 +0100

    Use edamame, simplify

--- src/clj2el/cli.clj
+++ src/clj2el/cli.clj
@@ -3,7 +3,10 @@
    [clj2el.api :refer [clj2el]]
    [clojure.edn :as edn]
    [clojure.pprint :refer [pprint]]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [edamame.core :as e]
+
+   ))
 
 (defn read-multiple-forms [s]
   (edn/read-string (str "[" s "]")))
@@ -16,7 +19,10 @@
 (defn pprint-str-multiple-forms [forms]
   (str/join "\n" (map pprint-str forms)))
 
-(defn -main [& args]
+#_
+(pprint (e/parse-string-all "(f) (g) (h)" {:all true}))
+
+(defn by-reinventing-edamame [args]
   (let [argset (set args)
         multiple? (contains? argset "--multiple") ;; assuming we don't want to depend on babashka.cli
         pretty? (contains? argset "--pretty")
@@ -27,3 +33,15 @@
                            (and (not multiple?) (not pretty?)) prn-str)
         clj2el-fn (if multiple? (partial map clj2el) clj2el)]
     (-> *in* slurp read-fn clj2el-fn write-str-fn print)))
+
+(defn sanely [args]
+  ;; avoid --multiple and --pretty for now
+  (let [read-str (fn [s] (e/parse-string-all s {:all true}))
+        clj2el-fn (partial map clj2el)
+        write-str pprint-str-multiple-forms]
+    (-> *in* slurp read-str clj2el-fn write-str print)))
+
+(defn -main [& args]
+  (sanely args)
+  #_
+  (by-reinventing-edamame args))
