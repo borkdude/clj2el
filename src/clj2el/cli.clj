c3cafe0586184333127d7091e1d5dfad6c9250db
Teodor Heggelund <git@teod.eu>
2023-03-21 14:23:33 +0100

    Reduce indirection
    Why tree functions when we need 2?

--- src/clj2el/cli.clj
+++ src/clj2el/cli.clj
@@ -8,11 +8,8 @@
 (defn pprint-str-multiple-forms [forms]
   (str/join "\n" (map #(with-out-str (pprint %)) forms)))
 
-(defn sanely [_args]
+(defn -main [& _args]
   (let [read-str (fn [s] (e/parse-string-all s {:all true}))
         clj2el-fn (partial map clj2el)
         write-str pprint-str-multiple-forms]
     (-> *in* slurp read-str clj2el-fn write-str print)))
-
-(defn -main [& args]
-  (sanely args))
